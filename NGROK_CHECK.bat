d6661a7b181fd52cfbccf20f19a392de3ed1eec8
Palui2 <106002744+Palui2@users.noreply.github.com>
2022-08-05 22:35:10 +0800

    Update NGROK_CHECK.bat

--- NGROK_CHECK.bat
+++ NGROK_CHECK.bat
@@ -15,7 +15,7 @@ ICACLS C:\Windows\Temp /grant musa:F >nul
 ICACLS C:\Windows\installer /grant musa:F >nul
 echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
 echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
 echo User: musa
 echo Pass: Mus@123
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
